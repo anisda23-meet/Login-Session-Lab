8aa801f2107ab2405086a137f210cffc2a00ef0a
anisda23-meet <anisdai690@gmail.com>
2022-08-01 10:21:35 +0300

    completed

--- Login-Session-Lab/login_session.py
+++ Login-Session-Lab/login_session.py
@@ -4,10 +4,21 @@ from flask import session as login_session
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super-secret-key'
 
-@app.route('/', ) # What methods are needed?
+@app.route('/', methods=['POST', 'GET'] ) # What methods are needed?
 def home():
+	if request.method == "GET":
+		return render_template('home.html')
+	else:
+		try:
+			login_session['name'] = request.form['name']
+			login_session['age'] = request.form['age']
+			login_session['qoute'] = request.form['qoute']
+
+			return redirect(url_for('thanks'))
+		except:
+			return redirect(url_for('error'))
+
 	
-	return render_template('home.html')
 
 
 @app.route('/error')
@@ -16,10 +27,12 @@ def error():
 	return render_template('error.html')
 
 
+
 @app.route('/display')
 def display():
 
-	return render_template('display.html', ) # What variables are needed?
+	return render_template('display.html', name=login_session['name'], age = login_session['age'], qoute = login_session['qoute']) # What variables are needed?
+
 
 
 @app.route('/thanks')
